b5b7bdee2aaad929da3148dc25db239016ee9dcc
Hulkar Sharif <hulkar.sharif@gmail.com>
2023-12-01 20:10:36 -0500

    Finish the update part

--- script.js
+++ script.js
@@ -19,6 +19,47 @@ class Storage {
         this.setContactLists(keptContactLists);
     }
 
+    openUpdatePopup(id) {
+        const existingContactLists = this.get();
+        const contact = existingContactLists.find(
+            (contact) => contact.id === id
+        );
+        document.getElementById("updateFirstName").value = contact.firstName;
+        document.getElementById("updateLastName").value = contact.lastName;
+
+        document.getElementById("updateEmail").value = contact.email;
+        document.getElementById("updateLinkedInProfile").value =
+            contact.linkedInProfile;
+        document.getElementById("updatePhoneNumber").value =
+            contact.phoneNumber;
+        document.getElementById("updatePopup").style.display = "block";
+    }
+
+    saveUpdatedContact() {
+        const existingContactLists = this.get();
+        const index = existingContactLists.findIndex(
+            (c) => c.email === document.getElementById("updateEmail").value
+        );
+        if (index !== -1) {
+            existingContactLists[index] = {
+                firstName: document.getElementById("updateFirstName").value,
+                lastName: document.getElementById("updateLastName").value,
+                email: document.getElementById("updateEmail").value,
+                linkedInProfile: document.getElementById(
+                    "updateLinkedInProfile"
+                ).value,
+                phoneNumber: document.getElementById("updatePhoneNumber").value
+            };
+            this.setContactLists(existingContactLists);
+            app.loadContacts();
+            this.closeUpdatePopup();
+        }
+    }
+
+    closeUpdatePopup() {
+        document.getElementById("updatePopup").style.display = "none";
+    }
+
     setContactLists(updatedContactLists) {
         localStorage.setItem(
             "contactLists",
@@ -26,15 +67,17 @@ class Storage {
         );
     }
 }
-
 const storage = new Storage();
 
-class ContactListsApp {
+class ContactListApp {
     static tableBody = document.querySelector(".contactLists");
+
+    deleteContact = (id) => {
+        storage.delete(id);
+    };
 
     addContact = (contact) => {
         storage.add(contact);
-        this.createContactLists();
     };
 
     getAll() {
@@ -53,7 +96,6 @@ class ContactListsApp {
 
         const idTd = document.createElement("td");
         idTd.innerText = id;
-        tr.appendChild(idTd);
 
         const firstNameTd = document.createElement("td");
         firstNameTd.innerText = firstName;
@@ -75,76 +117,97 @@ class ContactListsApp {
         phoneNumberTd.innerText = phoneNumber;
         tr.appendChild(phoneNumberTd);
 
-        const deleteIcon = document.createElement("span");
-        deleteIcon.innerHTML = "&times;";
-        deleteIcon.style.cursor = "pointer";
+        const buttonsTd = document.createElement("td");
 
-        deleteIcon.addEventListener("click", (e) => {
+        const editButton = document.createElement("button");
+        editButton.innerText = "Edit";
+        editButton.style.cursor = "pointer";
+        editButton.style.backgroundColor = "blue";
+        editButton.style.color = "#fff";
+        editButton.style.border = "none";
+        editButton.style.borderRadius = "5px";
+        editButton.style.padding = "8px 12px";
+        editButton.style.width = "50px";
+        editButton.style.marginRight = "12px";
+
+        editButton.addEventListener("click", () => {
+            storage.openUpdatePopup(id);
+            this.loadContacts;
+        });
+
+        const deleteButton = document.createElement("button");
+        deleteButton.innerText = "Delete";
+        deleteButton.style.cursor = "pointer";
+        deleteButton.style.backgroundColor = "#ff5555";
+        deleteButton.style.color = "#fff";
+        deleteButton.style.border = "none";
+        deleteButton.style.borderRadius = "6px";
+        deleteButton.style.padding = "10px 12px";
+        deleteButton.style.width = "65px";
+
+        deleteButton.addEventListener("click", (e) => {
             this.deleteContact(id);
+            this.loadContacts();
         });
 
-        tr.appendChild(deleteIcon);
-        ContactListsApp.tableBody.appendChild(tr);
-
-        const updateButton = document.createElement("button");
-        updateButton.innerText = "Update";
-        updateButton.addEventListener("click", (e) => {
-            this.updateContact(contact);
+        const saveButton = document.getElementById("saveButton");
+        saveButton.addEventListener("click", () => {
+            storage.saveUpdatedContact();
         });
-        tr.appendChild(updateButton);
+        const cancelButton = document.getElementById("cancelButton");
+        cancelButton.addEventListener("click", () => {
+            document.getElementById("updatePopup").style.display = "none";
+        });
+        buttonsTd.appendChild(editButton);
+        buttonsTd.appendChild(deleteButton);
+        tr.appendChild(buttonsTd);
+        ContactListApp.tableBody.appendChild(tr);
     };
-    createContactLists = () => {
-        ContactListsApp.tableBody.innerHTML = "";
-        const contactLists = this.getAll();
+
+    loadContacts = () => {
+        ContactListApp.tableBody.innerHTML = "";
+        const contactLists = storage.get();
         for (const contact of contactLists) {
             this.createContact(contact);
         }
     };
 
     initForm = () => {
-        const form = document.querySelector("form");
+        const form = document.querySelector("#contactForm");
 
         form.addEventListener("submit", (event) => {
             event.preventDefault();
+
             const id = crypto.randomUUID();
-            const firstName = document.querySelector("#firstNameInput").value;
-            const lastName = document.querySelector("#lastNameInput").value;
-            const email = document.querySelector("#emailInput").value;
-            const linkedInProfile = document.querySelector(
-                "#linkedInProfileInput"
-            ).value;
-            const phoneNumber =
-                document.querySelector("#phoneNumberInput").value;
+
+            const firstName = document.querySelector("#firstName").value;
+            const lastName = document.querySelector("#lastName").value;
+            const email = document.querySelector("#email").value;
+            const linkedInProfile =
+                document.querySelector("#linkedInProfile").value;
+            const phoneNumber = document.querySelector("#phoneNumber").value;
 
             const contact = {
                 id: id,
                 firstName: firstName,
                 lastName: lastName,
                 email: email,
-                linkedInProfile: linkedInProfile,
+                linkedIn: linkedInProfile, // Changed the field name
                 phoneNumber: phoneNumber
             };
 
             this.addContact(contact);
 
             form.reset();
+            this.loadContacts();
         });
     };
 
     init = () => {
-        this.createContactLists();
+        this.loadContacts();
         this.initForm();
-        const storedContacts = storage.get();
-        storedContacts.forEach((contact) => {
-            this.createContact(contact);
-        });
-    };
-
-    deleteContact = (id) => {
-        storage.delete(id);
-        this.createContactLists();
     };
 }
 
-const app = new ContactListsApp();
+const app = new ContactListApp();
 app.init();
